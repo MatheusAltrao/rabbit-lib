2030ca9814fbc2a400f6b80cd90b2ed38c6fe2fe
Matheus Altrao <matheusaltrao2@gmail.com>
2025-04-24 14:42:08 -0400

    ✨ chore(tailwind): Adiciona suporte a arquivos de configuração do `rabbit-lib` no `tailwind.config.js`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./node_modules/@matheusaltrao/rabbit-lib/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
